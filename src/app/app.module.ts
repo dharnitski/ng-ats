6e0a0c48bb1b7885977d5e921b9de79b82057444
Dmitry Harnitski <dmitry.harnitski@careerbuilder.com>
2020-01-05 16:54:27 -0500

    add empty header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { MaterialModule } from './material.module';
+import { MatDialogModule } from '@angular/material';
+import { HeaderComponent } from './navigation/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MaterialModule,
+    // dynamic component exception for Material Components
+    MatDialogModule,
     StoreModule.forRoot(reducers, {
       metaReducers,
       runtimeChecks: {
---
+++ src/app/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import {
+    MatButtonModule, MatIconModule, MatInputModule,
+    MatFormFieldModule, MatDatepickerModule, MatNativeDateModule,
+    MatCheckboxModule, MatSidenavModule, MatToolbarModule,
+    MatListModule, MatTabsModule, MatCardModule,
+    MatSelectModule, MatProgressSpinnerModule, MatDialogModule,
+    MatTableModule, MatSortModule, MatPaginatorModule,
+    MatSnackBarModule
+} from '@angular/material';
+
+const types = [MatButtonModule, MatIconModule, MatInputModule,
+    MatFormFieldModule, MatDatepickerModule, MatNativeDateModule,
+    MatCheckboxModule, MatSidenavModule, MatToolbarModule,
+    MatListModule, MatTabsModule, MatCardModule,
+    MatSelectModule, MatProgressSpinnerModule, MatDialogModule,
+    MatTableModule, MatSortModule, MatPaginatorModule,
+    MatSnackBarModule
+];
+
+@NgModule({
+    imports: types,
+    exports: types
+})
+export class MaterialModule { }
