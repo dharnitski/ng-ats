7a86f40eaa3dc8bd4a7618ea1cbca9408dc12ae0
Dmitry Harnitski <dmitry.harnitski@careerbuilder.com>
2020-01-06 10:33:16 -0500

    link to req details

--- src/app/requisitions/requisitions-routing.module.ts
+++ src/app/requisitions/requisitions-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { RequisitionsComponent } from './requisitions.component';
+import { RequisitionDetailsComponent } from './requisition-details/requisition-details.component';
 
-const routes: Routes = [{ path: '', component: RequisitionsComponent }];
+const routes: Routes = [
+  { path: '', component: RequisitionsComponent },
+  { path: 'req/:id', component: RequisitionDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
